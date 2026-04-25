f3becd399aa812fa7377576933b8e778935033ac
Zeeshan Abbasi <zeeshan@mparsec.com>
2026-04-25 13:17:47 +0500

    Fixed node modules compression

--- terraform/stg/variables.tf
+++ terraform/stg/variables.tf
@@ -72,10 +72,11 @@ variable "app_settings" {
   type        = map(string)
   description = "App Service application settings (merge runtime env, SCM flags, etc.)."
   default = {
-    "WEBSITE_NODE_DEFAULT_VERSION"        = "~20"
-    "SCM_DO_BUILD_DURING_DEPLOYMENT"      = "false"
-    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
-    "PORT"                                = "8080"
-    "NEXT_TELEMETRY_DISABLED"             = "1"
+    "WEBSITE_NODE_DEFAULT_VERSION"              = "~20"
+    "SCM_DO_BUILD_DURING_DEPLOYMENT"            = "false"
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE"       = "false"
+    "WEBSITE_SKIP_NODE_MODULES_TAR_COMPRESSION" = "1"
+    "PORT"                                      = "8080"
+    "NEXT_TELEMETRY_DISABLED"                   = "1"
   }
 }
